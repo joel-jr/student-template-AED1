011d73232edb76b5d0a5a45a5dadc6d925f50d22
joel-jr <45715660+joel-jr@users.noreply.github.com>
2024-04-01 17:18:52 +0000

    add funç. 'remove' + tests na main

--- w7-bst/bst.c
+++ w7-bst/bst.c
@@ -14,6 +14,7 @@ struct BST {
     Node *root;
 };
 
+// Create / Destroy:
 BST *bst_create() {
     BST *bst = malloc(sizeof(BST)); // alocar memória para a struct
     bst->root = NULL; // setar nó 'root': tirar possível 'lixo' guardado
@@ -35,8 +36,9 @@ void bst_destroy(BST *bst) {
     free(bst); // libear memória da struct
 }
 
+// Insert:
 Node *createNode(Element key) {
-    Node *node = malloc(sizeof(Node));
+    Node *node = malloc(sizeof(Node)); // alocar memória para nó criado
     node->key = key;
     node->left = NULL;
     node->right = NULL;
@@ -94,38 +96,90 @@ void bst_insert(BST *bst, Element key) {
     //bst_insertIter(bst, key);
 }
 
+// Remove:
+Node *bst_successor(Node *node) {
+    Node *cur = node->right; // acessar subárvore ->
+    while (cur->left != NULL)
+        cur = cur->left; // chegar nó folha <- dessa subárvore
+
+    return cur;
+}
+
+Node *bst_removeRecur(Node *node, Element key) {
+    // Caso Base1: árvore vazia ou chave NÃO encontrada
+    if (node == NULL)
+        return node;
+
+    // Chamadas Recursivas:
+    if (key < node->key)
+        node->left = bst_removeRecur(node->left, key); // acessar subárvore <-
+    else if (key > node->key)
+        node->right = bst_removeRecur(node->right ,key); // acessar subárvore ->
+
+    // Caso Base2: chave encontrada
+    else { // (key == node->key)
+        // Caso Base2.1 / 2.2: nó folha / NÃO possui filho <-
+        if (node->left == NULL) {
+            Node *trash = node; // copiar nó atual
+            node = node->right; // puxar / subst. nó atual pelo seu filho ->
+            free(trash); // liberar nó 'trash'
+        }
+        // Caso Base2.2: nó NÃO possui filho -> (puxar filho esq)
+        else if (node->right == NULL) {
+            Node *trash = node; // copiar nó atual
+            node = node->left; // puxar / subst. nó atual pelo seu filho <-
+            free(trash); // liberar nó 'trash'
+        }
+        // Caso Base2.3: nó possui 2 filhos
+        else {
+            Node *successor = bst_successor(node); // achar nó sucessor
+            node->key = successor->key; // subst. chave nó atual pelo sucessor
+            node->right = bst_removeRecur(node->right, successor->key); // eliminar nó sucessor
+        }
+    }
+
+    return node;
+}
+void bst_remove(BST *bst, Element key) {
+    bst->root = bst_removeRecur(bst->root, key);
+    // bst_removeIter é MT mais complicado!!!
+}
+
+// Search:
 bool bst_searchRecur(Node *node, Element key) { // busca RECURSIVAMENTE um elemento na ABB
-    // Casos Base:
-    if (node == NULL) // árvore vazia ou nó não encontrado
+    // Caso Base1: árvore vazia ou chave NÃO encontrada
+    if (node == NULL) 
         return false;
+    // Caso Base2: chave encontrada na árvore
     if (node->key == key)
         return true;
 
     // Chamadas Recursivas:
-    if (key < node->key) // acessar subárvore <-
-        return bst_searchRecur(node->left, key);
-    // (key > cur->key) acessar subárvore ->
-    return bst_searchRecur(node->right, key);
+    if (key < node->key)
+        return bst_searchRecur(node->left, key); // acessar subárvore <-
+    // (key > cur->key)
+    return bst_searchRecur(node->right, key); // acessar subárvore ->
 }
 bool bst_searchIter(Node *cur, Element key) { // busca ITERATIVAMENTE um elemento na ABB
     while (cur != NULL) {
         if (cur->key == key)
             return true;
         
-        // Verificar lado a percorrer (<- ESQ ou DIR ->):
+        // Verificar lado da árvore percorrer:
         if (key < cur->key)
             cur = cur->left; // acessar subárvore <-
         else
             cur = cur->right; // acessar subárvore ->
     }
     
-    return false; // árvore vazia ou nó não encontrado
+    return false; // árvore vazia ou chave NÃO encontrada
 }
 bool bst_search(BST *bst, Element key) {
     return bst_searchRecur(bst->root, key); // passar nó raíz da árvore
     //return bst_searchIter(bst->root, key); // passar nó raíz da árvore
 }
 
+// Prints:
 void bst_printInOrderRecur(Node *node) { // left, nó, right
     // Verificar se é nó folha:
     if (node == NULL)
--- w7-bst/main.c
+++ w7-bst/main.c
@@ -2,71 +2,6 @@
 
 #include "bst.h"
 
-void testPrints() {
-    BST *bst = bst_create();
-    Element keys[] = {5, 3, 7, 2, 4, 6, 8};
-
-    for (int i=0; i < 7; i++)
-        bst_insert(bst, keys[i]);
-
-    printf("•inOrder (odem crescente elementos):\n");
-    bst_printInOrder(bst);
-    printf("\n");
-
-    printf("•preOrder (ordem diagrama - cima/baixo):\n");
-    bst_printPreOrder(bst);
-    printf("\n");
-
-    printf("•diagram (preOrder):\n");
-    bst_print(bst);
-    printf("\n");
-
-    printf("•postOrder (ordem destroy - baixo/cima):\n");
-    bst_printPostOrder(bst);
-    printf("\n");
-
-    bst_destroy(bst);
-}
-
-void testInsert() {
-    BST *bst = bst_create();
-    // Árvore 1:
-    Element validKeys1[] = {50, 43, 3, 48, 45, 61, 58, 70};
-    Element invalidKeys1[] = {1, 5, 10, 90, 100};
-
-    for (int i=0; i < 8; i++)
-        bst_insert(bst, validKeys1[i]);
-
-    // Buscar elementos inseridos:
-    printf("•Árvore1:\n");
-    for (int i=0; i < 8; i++)
-        printf("%d: %s\n", validKeys1[i], bst_search(bst, validKeys1[i]) ? "true" : "false");
-
-    // Buscar elementos NÃO inseridos:
-    for (int i=0; i < 5; i++)
-        printf("%d: %s\n", invalidKeys1[i], bst_search(bst, invalidKeys1[i]) ? "true" : "false");
-    printf("\n");
-
-
-    // Árvore 2:
-    Element validKeys2[] = {5, 3, 7, 2, 4, 6, 8};
-    Element invalidKeys2[] = {0, 1, 9};
-
-    for (int i=0; i < 7; i++)
-        bst_insert(bst, validKeys2[i]);
-
-    // Buscar elementos inseridos:
-    printf("•Árvore2:\n");
-    for (int i=0; i < 7; i++)
-        printf("%d: %s\n", validKeys2[i], bst_search(bst, validKeys2[i]) ? "true" : "false");
-
-    // Buscar elementos NÃO inseridos:
-    for (int i=0; i < 3; i++)
-        printf("%d: %s\n", invalidKeys2[i], bst_search(bst, invalidKeys2[i]) ? "true" : "false");
-
-    bst_destroy(bst);
-}
-
 void printTree(BST *bst) {
     printf("•In-Order: ");
     bst_printInOrder(bst);
@@ -74,25 +9,135 @@ void printTree(BST *bst) {
     bst_print(bst);
     printf("\n•Post-Order: ");
     bst_printPostOrder(bst);
-    printf("\n");
+    printf("\n\n");
 }
 
-void testRoute() {
+void testRoutePrints() {
+    printf("<testRoutePrints()>\n\n");
     BST *bst = bst_create();
-    int validKeys[] = {50, 61, 58, 43, 70, 48, 3};
+    int elements[] = {50, 61, 58, 43, 70, 48, 3};
+
+    for (int i=0; i < 7; i++)
+        bst_insert(bst, elements[i]);
+
+    printTree(bst);
+    
+    bst_destroy(bst);
+}
+
+void testSearch() {
+    printf("<testSearch()>\n\n");
+    BST* bst = bst_create();
+    Element validKeys[] = {50, 43, 3, 48, 61, 58, 70};
+    Element invalidKeys[] = {0, 1, 2, 4, 5, 6, 7, 8, 9, 10, 90, 100};
 
     for (int i=0; i < 7; i++)
         bst_insert(bst, validKeys[i]);
+    
+    // Buscar elementos existentes:
+    for (int i = 0; i < 7; i++)
+        printf("%d: %sfound\n", validKeys[i], bst_search(bst, validKeys[i]) ? "" : "not ");
+    printf("\n");
 
-    printTree(bst);
+    // Buscas elementos NÃO existentes:
+    for (int i = 0; i < 12; i++)
+        printf("%d: %sfound\n", invalidKeys[i], bst_search(bst, invalidKeys[i]) ? "" : "not ");
+    printf("\n");
+
+    bst_destroy(bst);
+}
+
+void testInsert() {
+    printf("<testInsert()>\n\n");
+    // Árvore 1:
+    BST *bst1 = bst_create();
+    Element validKeys1[] = {50, 43, 3, 48, 45, 61, 58, 70};
+    Element invalidKeys1[] = {1, 5, 10, 90, 100};
+
+    for (int i=0; i < 8; i++)
+        bst_insert(bst1, validKeys1[i]);
+
+    // Buscar elementos inseridos:
+    printf("•Árvore1:\n");
+    for (int i=0; i < 8; i++)
+        printf("%d: %s\n", validKeys1[i], bst_search(bst1, validKeys1[i]) ? "true" : "false");
+
+    // Buscar elementos NÃO inseridos:
+    for (int i=0; i < 5; i++)
+        printf("%d: %s\n", invalidKeys1[i], bst_search(bst1, invalidKeys1[i]) ? "true" : "false");
+    printf("\n");
+
+
+    // Árvore 2:
+    BST *bst2 = bst_create();
+    Element validKeys2[] = {5, 3, 7, 2, 4, 6, 8};
+    Element invalidKeys2[] = {0, 1, 9};
+
+    for (int i=0; i < 7; i++)
+        bst_insert(bst2, validKeys2[i]);
+
+    // Buscar elementos inseridos:
+    printf("•Árvore2:\n");
+    for (int i=0; i < 7; i++)
+        printf("%d: %s\n", validKeys2[i], bst_search(bst2, validKeys2[i]) ? "true" : "false");
+
+    // Buscar elementos NÃO inseridos:
+    for (int i=0; i < 3; i++)
+        printf("%d: %s\n", invalidKeys2[i], bst_search(bst2, invalidKeys2[i]) ? "true" : "false");
+    printf("\n");
+
+    bst_destroy(bst1);
+    bst_destroy(bst2);
+}
+
+void testRemove() {
+    printf("<testRemove()>\n\n");
+    BST *bst = bst_create();
+    Element elements[] = {50, 43, 3, 48, 61, 58, 70};
+
+    for (int i = 0; i < 7; i++)
+        bst_insert(bst, elements[i]);
+    
+    printf("•Árvore original:\n");
+    bst_print(bst);
+    printf("\n");
+    bst_remove(bst, 3); // nó folha
+    printf("•bst_remove(3):\n");
+    bst_print(bst);
+    printf("\n");
+    bst_remove(bst, 70);
+    printf("•bst_remove(70):\n");
+    bst_print(bst);
+    printf("\n");
+    bst_remove(bst, 43); // nó APENAS 1 filho ->
+    printf("•bst_remove(43):\n");
+    bst_print(bst);
+    printf("\n");
+    bst_remove(bst, 61); // nó APENAS 1 filho <-
+    printf("•bst_remove(61):\n");
+    bst_print(bst);
+    printf("\n");
+    bst_remove(bst, 50); // nó 2 filhos (raiz)
+    printf("•bst_remove(50):\n");
+    bst_print(bst);
+    printf("\n");
+    bst_remove(bst, 48);
+    printf("•bst_remove(48):\n");
+    bst_print(bst);
+    printf("\n");
+    bst_remove(bst, 58);
+    printf("•bst_remove(58):\n(*)");
+    bst_print(bst);
+    printf("\n\n");
 
     bst_destroy(bst);
 }
 
 int main() {
-    //testPrints();
+    //testRoutePrints();
+    //testSearch();
     //testInsert();
-    //testRoute();
-
+    testRemove();
+    
     return 0;
 }
